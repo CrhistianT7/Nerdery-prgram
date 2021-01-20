860c2ffd913fe64ad6ca635066448c999564e90a
unknown <andrewturpo573@gmail.com>
2021-01-20 11:41:16 -0500

    module3 completed

---
+++ module3/playground/hoisting.js
@@ -0,0 +1,12 @@
+/** what does this file do? */
+sayHi();
+
+/** Hoew does this file do? */
+function sayHi(){
+    console.log("hey!");
+    console.log(add(10, 4))
+}
+
+function add(a,b) {
+    return a + b;
+}
---
+++ module3/playground/scope.js
@@ -0,0 +1,21 @@
+// function isCool(name) {
+//     let cool;
+//     if (name === "Andrew") {
+//         cool = true;
+//     }
+//     console.log(cool);
+//     return cool;
+// }
+
+const dog = 'snikers';
+
+function logDog() {
+    console.log(dog)
+}
+
+function go(){
+    const dog = 'sunny';
+    logDog();
+}
+
+go();
